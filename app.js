3c4af84e8fc020e868e8efffaf562b8bdc095b5a
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-06-27 08:25:03 +0545

    ui done

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
+//Require Express
 const express = require('express')
 const app = express()
-require('dotenv').config();
 
-//ejs buj
+//json buj
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//require dotenv
+require('dotenv').config()
+
+//set view engine
 app.set('view engine', 'ejs')
 
 //css buj
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.render('home', { data: 'Hello World!' })
+//get all blogs in home page
+app.get("/", (req,res)=>{
+    res.render('blogs')
 })
 
-app.get('/about', (req, res) => {
-  res.render('about', { data: 'About Us' })
+//get add blog form page
+app.get("/blog", (req,res)=>{
+    res.render('createBlog')
 })
+
+//create blog
+app.post("/blog", (req,res)=>{
+    console.log(req.body)
+})
+
+//update blog
+app.patch("/blog/:id", (req,res)=>{
+    res.render('singleBlog')
+})
+
+// delete blog
+app.delete("/blog/:id", (req,res)=>{
+    res.render('singleBlog')
+})
+
+
 
 const port = process.env.PORT
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+app.listen(port, (req,res)=>{
+    console.log(`Project successfully started at ${port}`)
 })
